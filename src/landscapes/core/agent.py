b406af11fab206bc7f157f358d648431637e7ddb
lejin <lejinsnests@gmail.com>
2025-12-23 22:12:18 -0500

    async agent running
    Signed-off-by: lejin <lejinsnests@gmail.com>

--- src/landscapes/core/agent.py
+++ src/landscapes/core/agent.py
@@ -1,10 +1,9 @@
+import asyncio
 import click
-from dotenv import load_dotenv
 
 from agno.agent import Agent
-from agno.models.anthropic import Claude
 from agno.models.ollama import Ollama
-load_dotenv()
+
 
 
 class BaseAgent:
@@ -12,20 +11,28 @@ class BaseAgent:
     Base class for an agent.
     """
 
-    def __init__(self, id: str, name: str, instructions: str, model_name: str):
+    def __init__(
+        self,
+        id: str,
+        name: str,
+        instructions: str,
+        model_name: str,
+        debug: bool,
+    ):
         self._id = id
         self._name = name
         self._instructions = instructions
         self._model_name = model_name
+        self._debug = debug
         self._model = self._create_model()
         self._agent = self._create_agent()
-    
+
     def _create_model(self):
         """Create a model instance."""
         return Ollama(
             id=self._model_name,
             options={
-                "temperature": 0.0,
+                "temperature": 0.1,
                 "top_p": 0.8,
                 "top_k": 20,
                 "repeat_penalty": 1.0,
@@ -41,27 +48,25 @@ class BaseAgent:
             model=self._model,
             markdown=True,
             system_message=self._instructions,
-            #tools
+            # tools
             compress_tool_results=True,
             tools=[],
             tool_call_limit=5,
-            #state
+            # state
             enable_agentic_state=True,
             enable_agentic_memory=True,
-            #session
+            # session
             add_session_state_to_context=True,
             search_session_history=True,
             cache_session=True,
             enable_session_summaries=True,
             add_session_summary_to_context=True,
-            #history
+            # history
             add_history_to_context=True,
             num_history_sessions=5,
-            #reasoning
-            reasoning=True,
-            reasoning_min_steps=1,
-            reasoning_max_steps=5
-
+            # events
+            store_events=True,
+            debug_mode=self._debug,
         )
 
     @property
@@ -79,22 +84,22 @@ class BaseAgent:
         """The instructions for the agent."""
         return self._instructions
 
-    def run(self, instruction: str, **kwargs) -> str:
+    async def run(self, instruction: str, **kwargs) -> str:
         """Execute the agent's logic."""
-        response = self._agent.run(instruction, **kwargs)
+        response = await self._agent.arun(instruction, **kwargs)
         return response.to_dict()
 
 
-if __name__ == "__main__":
+async def run_agent(task: str, debug: bool):
+    """Async logic for running the agent."""
     agent = BaseAgent(
         id="1",
         name="Agent 1",
         instructions="You are a helpful assistant.",
-        # model_name="claude-sonnet-4-5",
         model_name="qwen3:1.7b",
-
+        debug=debug,
     )
-    response = agent.run("What is the best value of humanity?")
+    response = await agent.run(task)
     attributes = [
         "run_id",
         "agent_id",
@@ -109,3 +114,19 @@ if __name__ == "__main__":
         if k not in attributes:
             continue
         click.secho(f"{k}: {v}", fg=colors[i % len(colors)])
+
+
+@click.command()
+@click.option(
+    "--task",
+    type=str,
+    required=True,
+    default="What is the best value of humanity, and why?",
+)
+@click.option("--debug", type=bool, required=False, default=False)
+def main(task: str, debug: bool):
+    asyncio.run(run_agent(task, debug))
+
+
+if __name__ == "__main__":
+    main()
